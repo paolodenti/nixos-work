bd09dafd446d559b34872573adb389c3b3e001f6
Paolo Denti <paolo.denti@gmail.com>
2024-09-15 15:43:26 -0700

    sops

--- profiles/gnome/configuration.nix
+++ profiles/gnome/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, sops-nix hostname, timezone, username, fullname, ... }:
+{ config, pkgs, lib, sops-nix, hostname, timezone, username, fullname, ... }:
 
 {
   imports =
